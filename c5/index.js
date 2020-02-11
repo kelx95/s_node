4154cccb3ad699fb8daa30d38df1f59a61511e24
kelmentxh <60695660+kelmentxh@users.noreply.github.com>
2020-02-11 16:43:09 +0100

    class mongoose

--- c5/index.js
+++ c5/index.js
@@ -1,29 +1,71 @@
-const mongoose = require('mongoose');
-const cstring = 'mongodb+srv://kelmentxh:xhelili123@cluster0-apiex.mongodb.net/test?retryWrites=true&w=majority';
 
-const produkti = mongoose.model(
-    'produkti',
-    {
-        ime: String,
-        proizvoditel: String,
-        cena: Number,
-        tezina: Number,
-        parcinja: Number
-    }
-);
 
-mongoose.connect(cstring, { useNewUrlParser: true }, (err)=>{
-    if(err){
-        console.log("Could not connect to database");
-        return;
-    }
-});
+// //add to database
+// let p = new Produkt({
+//     "ime": "Milka so Oreo",
+//     "proizvoditel": "Mondelez",
+//     "cena": 30.0,
+//     "tezina": 300.0,
+//     "parcinja": 1
+// });
 
-produkti.find({}, (err, data) => {
-    if (err) {
-        console.log('Could not read from database');
-        return;
-    }
-    console.log(data);
-});
+// p.save((err) => {
+//     if (err) {
+//         console.log('Could not connect to database');
+//         return;
+//     }
+// })
 
+// //read from database
+// Produkt.find({}, (err, data) => {
+//     if (err) {
+//         console.log('Could not read from database');
+//         return;
+//     }
+//     console.log(data);
+// });
+
+// //delete from database
+// Produkt.deleteOne({_id: "5e42c48284192e45746dd473" }, (err) => {
+//     if (err) {
+//         console.log('Could not delete from database');
+//         return;
+//     }
+// });
+
+// Produkt.update({ime: "Smoki"}, {cena: 15.0}, (err)=>{
+//     if(err){
+//         console.log('Could not update record');
+//         return;
+//     }
+// });
+
+const db = require('./bootstrap/db');
+const Produkt = require('./models/produkt');
+
+db.initDB();
+
+produkt.createNew({
+        "ime" : "Milka so jagodi",
+        "proizvoditel" : "Mondelez",
+        "cena" : 30,
+        "tezina" : 300,
+        "parcinja" : 1,
+        "__v" : 0   
+})
+    .then(()=>{
+        return produkt.update("", {ime: "Milka so Leshnici"});
+
+    })
+    .then(()=>{
+        return produkt.remove("");
+    })
+    .then(data => {
+        return produkt.readAll();
+    })
+    .then(()=>{
+        console.log(data);
+    })
+    .then((err)=>{
+        console.log(err);
+    })

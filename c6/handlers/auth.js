43a646ab6df31cfce7c05ddf42155d47131a1df3
kelmentxh <60695660+kelmentxh@users.noreply.github.com>
2020-02-21 14:13:51 +0100

    jwt

--- c6/handlers/auth.js
+++ c6/handlers/auth.js
@@ -1,6 +1,9 @@
 var bcrypt = require('bcryptjs');
 var user = require('../models/user');
 
+var jwt = require('jsonwebtoken');
+const tokenKey = 'pwd123!';
+
 const viewLogin = (req, res) => {
     res.render('login');
 }
@@ -12,6 +15,8 @@ const apiLogin = (req, res) => {
         user.getByEmail(req.body.email)
             .then(data => {
                 if(bcrypt.compareSync(req.body.password, data.password)) {
+                    let token = jwt.sign({ email: data.email }, tokenKey);
+                    res.cookie('jwt', token);
                     res.redirect('/dashboard');
                 } else {
                     res.redirect('/?err=1')

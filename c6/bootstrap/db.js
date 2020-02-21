6364e1f33906060f5c1f40e4736535e6e8cd90d0
Kelment <kelmentdjelili@gmail.com>
2020-02-21 11:05:50 +0100

    c6

---
+++ c6/bootstrap/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const cstring = 'mongodb+srv://kelmentxh:xhelili123@cluster0-apiex.mongodb.net/prodavnica?retryWrites=true&w=majority';
+
+
+const initDB = () => {
+    mongoose.connect(cstring, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+        if(err){
+            console.log('Could not connect to database');
+            return;
+        }
+    });
+}
+
+module.exports = {
+    initDB,
+};
